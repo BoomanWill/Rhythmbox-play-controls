e36a9d76f8707354e59d7a87362d6af9b2b29216
BoomanWill <76132689+BoomanWill@users.noreply.github.com>
2021-12-31 14:30:44 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo mkdir ~/.rhythmboxcontrols
+sudo mv images ~/.rhythmboxcontrols
+sudo mv controls.py ~
+pip install tkinter
+sudo pip install tkinter
+pip install os
+sudo pip install os
+pip install subprocess
+sudo pip install subprocess
+
